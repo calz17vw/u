fd38e3c2631bdb035c6a0087217447511bb85159
calz17vw <waleeed.1.4u@gmail.com>
2025-11-05 20:50:11 +0300

    Add files via upload

---
+++ tokens.js
@@ -0,0 +1,21 @@
+// status can be "online", "idle", "dnd", or "invisible" or "offline"
+export default [
+ 
+      {
+        channelId: "1433402598806781952",
+        serverId: "1383857733706584064",
+        token: "ODY2MjMzMTUwMDAxNjQzNTIw.GjMKpB.er32xGvO73sr5h6pSnz2q8D4_m9mQ8JG4syu44",
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "idle",
+        },
+        selfMute: false,
+        
+    },
+    
+];
