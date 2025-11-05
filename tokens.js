df78247b34f61bb54a25d030c49c30b250b90163
calz17vw <waleeed.1.4u@gmail.com>
2025-11-05 21:09:10 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,10 +1,9 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
- 
-      {
+    {
         channelId: "1433402598806781952",
         serverId: "1383857733706584064",
-        token: "ODY2MjMzMTUwMDAxNjQzNTIw.GjMKpB.er32xGvO73sr5h6pSnz2q8D4_m9mQ8JG4syu44",
+        token: process.env.token1,
         selfDeaf: false,
         autoReconnect: {
             enabled: true,
@@ -14,8 +13,6 @@ export default [
         presence: {
             status: "idle",
         },
-        selfMute: false,
-        
+        selfMute: true,
     },
-    
 ];
